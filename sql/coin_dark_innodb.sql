91ba6219914752216dde44ba5ba828581b8cd78d
gongyali <gongyali@oradt.com>
2018-08-17 08:52:20 +0800

    edit sql
    Change-Id: I9165707eec98254891daaa8541b6f8c65ebf17be

--- sql/coin_dark_innodb.sql
+++ sql/coin_dark_innodb.sql
@@ -10,7 +10,7 @@ Target Server Type    : MYSQL
 Target Server Version : 50722
 File Encoding         : 65001
 
-Date: 2018-08-07 09:52:23
+Date: 2018-08-17 08:49:28
 */
 
 SET FOREIGN_KEY_CHECKS=0;
@@ -20,9 +20,9 @@ SET FOREIGN_KEY_CHECKS=0;
 -- ----------------------------
 DROP TABLE IF EXISTS `block`;
 CREATE TABLE `block` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `block_hash` char(64) NOT NULL,
   `previous_block_hash` char(64) DEFAULT NULL,
-  `next_block_hash` char(64) DEFAULT NULL,
   `merkleroot` char(64) DEFAULT NULL,
   `chainwork` char(64) DEFAULT NULL,
   `size` bigint(20) DEFAULT NULL,
@@ -36,36 +36,39 @@ CREATE TABLE `block` (
   `status` tinyint(3) NOT NULL DEFAULT '1' COMMENT '0:无效，1:有效',
   `create_ts` datetime DEFAULT NULL,
   `update_ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`block_hash`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `block_hash_UNIQUE` (`block_hash`) USING BTREE,
   KEY `idx_height` (`height`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=23 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for input
 -- ----------------------------
 DROP TABLE IF EXISTS `input`;
 CREATE TABLE `input` (
-  `block_hash` char(64) DEFAULT NULL,
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `transaction_txid` char(64) NOT NULL COMMENT '当前vin所属transaction',
+  `wallet_account_id` bigint(20) DEFAULT NULL,
+  `address` varchar(512) DEFAULT NULL,
   `txid` char(64) DEFAULT NULL,
   `coinbase` varchar(512) DEFAULT NULL,
   `vout` int(11) DEFAULT NULL,
   `sequence` bigint(20) DEFAULT NULL,
-  `script_sig_hex` mediumtext,
-  `script_sig_asm` text,
-  `status` tinyint(3) NOT NULL DEFAULT '1' COMMENT '0:无效，1:有效',
+  `status` tinyint(3) NOT NULL DEFAULT '1',
   `create_ts` datetime DEFAULT NULL,
-  `update_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  `update_ts` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=372 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for output
 -- ----------------------------
 DROP TABLE IF EXISTS `output`;
 CREATE TABLE `output` (
+  `id` bigint(20) NOT NULL AUTO_INCREMENT,
   `wallet_account_id` bigint(20) DEFAULT NULL,
-  `block_hash` char(64) DEFAULT NULL,
   `transaction_txid` char(64) NOT NULL COMMENT '当前vout所属transaction',
+  `coinbase` tinyint(3) NOT NULL DEFAULT '0' COMMENT '0:false,1:true',
   `value` double DEFAULT NULL,
   `value_sat` bigint(20) DEFAULT NULL,
   `n` int(11) DEFAULT NULL,
@@ -74,17 +77,21 @@ CREATE TABLE `output` (
   `script_pub_key_req_sigs` int(11) DEFAULT NULL,
   `script_pub_key_type` char(64) DEFAULT NULL,
   `script_pub_key_addresses` varchar(512) DEFAULT NULL,
-  `status` tinyint(3) NOT NULL DEFAULT '1' COMMENT '0:无效，1:有效',
+  `status` tinyint(3) NOT NULL DEFAULT '1',
   `create_ts` datetime DEFAULT NULL,
   `update_ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  UNIQUE KEY `idx_txid_n` (`transaction_txid`,`n`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `idx_txid_n` (`transaction_txid`,`n`) USING BTREE,
+  KEY `idx_address` (`script_pub_key_addresses`) USING BTREE,
+  KEY `idx_status` (`status`) USING BTREE
+) ENGINE=InnoDB AUTO_INCREMENT=617 DEFAULT CHARSET=utf8;
 
 -- ----------------------------
 -- Table structure for transaction
 -- ----------------------------
 DROP TABLE IF EXISTS `transaction`;
 CREATE TABLE `transaction` (
+  `id` bigint(11) NOT NULL AUTO_INCREMENT,
   `txid` char(64) NOT NULL,
   `hex` mediumtext,
   `size` bigint(20) DEFAULT NULL,
@@ -92,11 +99,13 @@ CREATE TABLE `transaction` (
   `locktime` bigint(20) DEFAULT NULL,
   `height` bigint(20) DEFAULT NULL,
   `time` bigint(20) DEFAULT NULL,
-  `block_hash` char(64) NOT NULL,
+  `block_hash` char(64) DEFAULT NULL,
   `block_time` bigint(20) DEFAULT NULL,
+  `trans_status` tinyint(3) NOT NULL COMMENT '1.sent,2.packed',
   `status` tinyint(3) NOT NULL DEFAULT '1' COMMENT '0:无效,1:有效',
   `create_ts` datetime DEFAULT NULL,
   `update_ts` timestamp NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
-  PRIMARY KEY (`txid`),
+  PRIMARY KEY (`id`),
+  UNIQUE KEY `txid_UNIQUE` (`txid`) USING BTREE,
   KEY `idx_block_hash` (`block_hash`) USING BTREE
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=255 DEFAULT CHARSET=utf8;
